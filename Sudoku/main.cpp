c5e4eae0857ec2dad282a83bb5449332659f71ac
Matt Matney <mattbmat@gmail.com>
2015-03-15 00:13:05 -0400

    initial commit for sudoku

---
+++ Sudoku/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Matt
+ *
+ * Created on March 15, 2015, 12:09 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
